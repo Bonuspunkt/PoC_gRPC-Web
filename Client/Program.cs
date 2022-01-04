c5ccbbacafd60b46c2682cc650e0f1bbab260d5a
Bonuspunkt <bonuspunkt+github@gmail.com>
2022-01-04 08:40:11 +0100

    init

---
+++ Client/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+using Greet;
+using Grpc.Net.Client;
+using Grpc.Net.Client.Web;
+
+var version = RuntimeInformation.FrameworkDescription;
+
+var handler = new GrpcWebHandler(new HttpClientHandler());
+var options = new GrpcChannelOptions {HttpHandler = handler};
+
+
+var channel = GrpcChannel.ForAddress("http://localhost:5001", options);
+var client = new Greeter.GreeterClient(channel);
+
+var result = await client.SayHelloAsync(new HelloRequest { Name = version });
+Console.WriteLine(result.Message);
